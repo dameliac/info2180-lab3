155f7214e693059506b191d142efde038b56a9e8
Damelia Coleman <dameliac@outlook.com>
2023-10-22 08:17:18 -0500

    Added X or O to a square when clicked.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,47 @@
 window.onload=function myFunction(){
-    const status = document.getElementById("status");
+    const status = document.getElementById("status"); 
     const board  = document.getElementById("board");
-    const newgame = document.getElementsByClassName("btn");
-    const shapes = board.querySelectorAll('div');
+    const newgame = document.getElementsByClassName("btn")[0];
+    let shapes = board.querySelectorAll('div');
+    //let shapes = new Array();
+    let gamestate = new Array ();
+    let currentplay = 'X';
     
-    for (let i = 0; i <= 9; i++){
-        shapes[i].setAttribute("class", "square");
+    
+    
+    
+    for (let i = 0; i <= 8; i++){
+        shapes[i].classList.add('square');
     }
+    //console.log(shapes);
     
+    shapes.forEach(function(elem,index){
+        elem.addEventListener('click', function(){
+            GameBoard(elem,index)});
+        elem.addEventListener('mouseover', function(i){
+            i.target.classList.add('hover');
+        });
+        elem.addEventListener('mouseout', function(i){
+            i.target.classList.remove('hover');
+        });
+    });
+    
+
+    function GameBoard(elem, index){ 
+        console.log(elem.textContent);       
+        if (!elem.textContent){
+            elem.textContent = currentplay;
+            elem.classList.add('square', currentplay); //Add a class for colour.
+            //elem[index].textContent.style.classList.add('square.X');
+            gamestate[index] = currentplay;
+            console.log(gamestate);
+            currentplay = currentplay === 'X' ? 'O': 'X';
+         
+        }
+    }
+
+
+
 
 }
 
